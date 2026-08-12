624d7a06303bf9780eebe798dca76f645608372a
Mustafaaa123aldamrawi <mustafa@maktabitech.com>
2026-08-12 12:20:07 +0300

    Upgrade LLM to FLAN-T5 Base for improved document QA

--- app.py
+++ app.py
@@ -76,8 +76,7 @@ def create_vector_store(text_chunks):
 @st.cache_resource
 def load_llm():
     """Load the FLAN-T5 tokenizer and model."""
-    model_name = "google/flan-t5-small"
-
+    model_name = "google/flan-t5-base"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
